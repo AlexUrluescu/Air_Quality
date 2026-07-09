13c1ee8627a4af5f213a5268874209c84010ccb0
AlexUrluescuWork <alexandre.urluescu@mtdtechnology.net>
2026-07-09 16:03:04 +0300

    update the .sh file

--- start.sh
+++ start.sh
@@ -18,14 +18,5 @@ uv run python app.py &
 BACKEND_PID=$!
 
 echo "Se încarcă modelul de Machine Learning..."
-sleep 3
-
-echo "Se pornește Dashboard-ul pe portul 5002..."
-uv run panel serve dashboard.py --show --port 5002 &
-FRONTEND_PID=$!
-
-echo "Ambele servere rulează acum în paralel!"
-echo "Accesează interfața la: http://localhost:5002"
-echo "Pentru a opri tot sistemul, apăsați CTRL+C în acest terminal."
 
 wait
